e952bfccaa7bc0d517bcd807c3025ae27d64fcaf
SnirGa <64706615+SnirGa@users.noreply.github.com>
2021-10-14 21:27:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace DistributedSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Boolean B = false;
+            Boolean br = false;
+            CancellationTokenSource source = new CancellationTokenSource();
+            CancellationToken token = source.Token;
+            ThreadSearcher T1;
+           // using (StreamReader SR = new StreamReader("textfile.txt"))
+            using (StreamReader SR = new StreamReader(args[0]))
+
+            {
+                char[] buffer = new char[10000];
+
+                //T1 = new ThreadSearcher(2, "textfile.txt", "OW", buffer);
+                int d = Int32.Parse(args[3]);
+                T1 = new ThreadSearcher(d+1, args[0], args[1], buffer);
+                int found = -1;
+                int blocknumber = 0;
+                int numofthreads= Int32.Parse(args[2]);
+                 ThreadPool.SetMinThreads(numofthreads, 0);
+               ThreadPool.SetMaxThreads(numofthreads, 0);
+                // ThreadPool.SetMinThreads(1, 0);
+                //ThreadPool.SetMaxThreads(1, 0);
+                for (int i = 0; i < numofthreads; i++)
+                {
+                    while (!SR.EndOfStream)
+                    {
+                        SR.ReadBlock(buffer, 0, 10000);
+                        T1.setBuffer(buffer);
+                        ThreadPool.QueueUserWorkItem(ThreadSearcher.Search, T1);
+                        Thread.Sleep(100);
+                        if (T1.getIndex() != -1)
+                        {
+                            found = T1.getIndex() + blocknumber * 10000;
+                            br = true;
+                            break;
+                        }
+                        blocknumber++;
+                    }
+                    if (br == true)
+                    {
+                        break;
+                    }
+                }
+                if (found != -1)
+                {
+                    Console.WriteLine(found);
+                }
+                else
+                {
+                    Console.WriteLine("not found");
+
+                }
+
+
+            }
+        }
+    }
+}
---
+++ ThreadSearcher.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedSearch
+{
+    class ThreadSearcher
+    {
+        int del;
+        String text;
+        String TS;
+        char[] buff;
+        int startindex = -1;
+        int id;
+
+        public ThreadSearcher(int d, String txt, String ts, char[] buffer)
+        {
+            text = txt;
+            TS = ts;
+            del = d;
+            buff = buffer;
+
+
+        }
+        public void run()
+        {
+            Search(this);
+        }
+        public String getStringToSearch()
+        {
+            return TS;
+        }
+        public int getDelta()
+        {
+            return del;
+        }
+        public String getText()
+        {
+            return text;
+        }
+        public void setIndex(int n)
+        {
+            this.startindex = n;
+        }
+        public int getIndex()
+        {
+            return startindex;
+        }
+        public void setId(int ID)
+        {
+            this.id = ID;
+        }
+        public char[] getBuffer()
+        {
+            return this.buff;
+        }
+        public void setBuffer(char[] b)
+        {
+            this.buff = b;
+        }
+        public static void Search(object o)
+        {
+
+            int index = -1;
+
+            ThreadSearcher t = (ThreadSearcher)o;
+            int delta = ((ThreadSearcher)o).getDelta();
+
+
+            int buffernumber = 0;
+            
+            String subString = ((ThreadSearcher)o).getStringToSearch();
+
+
+            String location = ((ThreadSearcher)o).getText();
+
+
+            using (StreamReader r = new StreamReader(location))
+            {
+                char[] buffer = ((ThreadSearcher)o).getBuffer();
+
+                for (int i = 0; i < buffer.Length; i++)
+                {
+                    if (index != -1)
+                    {
+
+                        break;
+                    }
+                    Boolean found = true;
+                    int count = 0;
+                    for (int j = i; j < i + ((delta) * subString.Length); j += delta)
+                    {
+                        found = true;
+                        if (j < 10000)
+                        {
+                            if (subString[count++] != buffer[j])
+                            {
+
+                                found = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (found)
+                    {
+                        index = i;
+
+                        break;
+                    }
+
+
+                }
+
+            }
+            
+
+            ((ThreadSearcher)o).setIndex(index);
+        }
+    }
+}
+
